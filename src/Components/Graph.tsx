c8dc06dd97f7866c42058875c59b059b9f49af6c
reniz-shah <reniz2810@gmail.com>
2024-09-20 13:09:20 +0530

    add new values and made responsive

--- src/Components/Graph.tsx
+++ src/Components/Graph.tsx
@@ -7,20 +7,22 @@ const Graph: React.FC<any> = ({
 
     return (
         <>
-            <LineChart
-                className='Graph'
-                xAxis={[{ data: x, label: "Ticks" }]}
-                series={[
-                    {
-                        data: y,
-                        showMark: false,
-                    },
-                ]}
-                height={600}
-                width={900}
-                margin={{ left: 30, right: 30, top: 30, bottom: 60 }}
-                grid={{ vertical: true, horizontal: true }}
-            />
+            <div style={{overflow:'scroll'}}>
+                <LineChart
+                    className='Graph'
+                    xAxis={[{ data: x, label: "Ticks" }]}
+                    series={[
+                        {
+                            data: y,
+                            showMark: false,
+                        },
+                    ]}
+                    height={600}
+                    width={900}
+                    margin={{ left: 35, right: 30, top: 30, bottom: 60 }}
+                    grid={{ vertical: true, horizontal: true }}
+                />
+            </div>
         </>
     )
 }
